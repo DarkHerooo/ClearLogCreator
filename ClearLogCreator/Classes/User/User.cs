eb1f83fd595a21b357304d192846a95c96cc1304
Артём Коршунов <korshun.herooo@yandex.ru>
2022-02-13 13:08:30 +0300

    V. 1.0.1

---
+++ ClearLogCreator/Classes/User/User.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClearLogCreator.Classes.User
+{
+    public static class User
+    {
+        public static UserSettings UserSettings { get; set; }
+
+        private static string _fileName = "../../Data/user_settings.json";
+        public static void SetUser()
+        {
+            if (File.Exists(_fileName)) 
+                UserSettings = JsonConvert.DeserializeObject<UserSettings>(File.ReadAllText(_fileName));
+            else
+            {
+                UserSettings = new UserSettings();
+                UserSettings.MinecraftLogsFolder = 
+                    Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\.minecraft\logs";
+                UserSettings.DeleteUselessFilesInMinecraftFolder = false;
+                UserSettings.DeleteUselessFilesInMyFolder = false;
+            }
+        }
+
+        public static void SaveUser()
+        {
+            File.WriteAllText(_fileName, JsonConvert.SerializeObject(UserSettings));
+        }
+    }
+}
---
+++ ClearLogCreator/Classes/User/UserSettings.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClearLogCreator.Classes.User
+{
+    public class UserSettings
+    {
+        public string MinecraftLogsFolder;
+        public string MyLogsFolder;
+        public bool DeleteUselessFilesInMinecraftFolder;
+        public bool DeleteUselessFilesInMyFolder;
+    }
+}
